b349d86046c582e7240f15b95af84dfe85631029
ovr1 <rybynok@mail.ru>
2019-08-04 22:31:33 +0300

    all

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -2,8 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from tasks import views
 
 urlpatterns = [
+    path('', views.Index, name='index'),
     path("tasks/", include("tasks.urls", namespace="tasks")),
     path("accounts/", include("accounts.urls")),
     path("admin/", admin.site.urls),
